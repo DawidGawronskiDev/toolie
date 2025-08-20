ca765d3b522f48517e392320709c90734f04aae7
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-08-20 13:52:54 +0100

    git add gui quit button

--- gui/src/components/header.tsx
+++ gui/src/components/header.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Power } from "lucide-react";
+
 const Header = () => {
   return (
     <header className="container py-4">
-      <h1>Toolie</h1>
+      <div className="w-full flex items-center justify-between">
+        <h1>Toolie</h1>
+        <Button>
+          <Power /> Quit
+        </Button>
+      </div>
     </header>
   );
 };
