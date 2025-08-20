2ae039af25daa4f4514747a32198d339f7a271f2
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-08-20 16:38:57 +0100

    add gui cards animation

--- gui/src/components/options/options-card.tsx
+++ gui/src/components/options/options-card.tsx
@@ -8,10 +8,14 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import type { HTMLAttributes } from "react";
 
-const OptionsCard = ({ option }: { option: Option }) => {
+const OptionsCard = ({
+  option,
+  ...props
+}: { option: Option } & HTMLAttributes<HTMLDivElement>) => {
   return (
-    <Card className="justify-between">
+    <Card className="justify-between animate-appear opacity-0" {...props}>
       <CardHeader>
         <CardTitle>{option.title}</CardTitle>
         <CardDescription>{option.description}</CardDescription>
--- gui/src/components/options/options-cards.tsx
+++ gui/src/components/options/options-cards.tsx
@@ -5,7 +5,11 @@ const OptionsCards = ({ options }: { options: Option[] }) => {
   return (
     <div className="grid grid-cols-[repeat(auto-fill,minmax(255px,1fr))] gap-4">
       {options.map((option, index) => (
-        <OptionsCard key={index} option={option} />
+        <OptionsCard
+          style={{ animationDelay: `${index * 50}ms` }}
+          key={index}
+          option={option}
+        />
       ))}
     </div>
   );
